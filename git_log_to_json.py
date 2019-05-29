116743d96b9d01f47f116f6018d437a07bbf57ed
Sunil.Jacob <sunil1405@gmail.com>
2019-05-29 12:30:12 +0530

    git2json

---
+++ git_log_to_json.py
@@ -0,0 +1,32 @@
+import sys
+import importlib.util
+from subprocess import Popen, PIPE
+import git2json
+
+'''
+This module use git2json package to transform the 
+output from the `git log` command into json format
+
+The output is written to a file after removing EOL
+characters
+'''
+PACKAGE_NAMES = ['git2json']
+for PACKAGE_NAME in PACKAGE_NAMES:
+    spec = importlib.util.find_spec(PACKAGE_NAME)
+    if spec is None:
+        print(PACKAGE_NAME+' not installed')
+        sys.exit()
+PROGRAM_NAME = sys.argv[0]
+GIT_DIR = sys.argv[1:]
+
+if len(GIT_DIR) != 1:
+    print('Argument missing:')
+    print('      Usage: python {} {}'.format(sys.argv[0], 'git directory'))
+    sys.exit()
+
+MODULE_NAME = 'git2json'
+PROCESS = Popen(MODULE_NAME, cwd=GIT_DIR[0], stdout=PIPE, stderr=PIPE)
+(PROCESS, ERROR) = PROCESS.communicate()
+REMOVE_BYTE_LITERAL = PROCESS.decode('utf-8')
+with open('out.json', 'w') as f:
+    print(REMOVE_BYTE_LITERAL, file=f)
