e262d278af6156021bd7f125acfb57183158ee6e
Sunil.Jacob <sunil1405@gmail.com>
2019-05-30 12:00:03 +0530

    Adding tests

--- tests/git_log_to_json_test.py
+++ tests/git_log_to_json_test.py
@@ -3,6 +3,6 @@ import pytest
 from subprocess import Popen, PIPE
 
 def test_output_of_parser():
-    program = 'git_log_to_json.py'
+    program = 'src/git_log_to_json.py'
     output = Popen([program],stdout=PIPE).communicate()[0]
     assert b'git_log_to_json.py\r\nArgument missing:\r\n' in output
